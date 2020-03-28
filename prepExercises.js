bf6106cc25d7014e9c7c0a9a28e1f6219a5fafba
CharafChroudi <charafchroudi17@gmail.com>
2020-03-28 12:25:27 +0100

    Solved question 2,3,4,5

--- prepExercises.js
+++ prepExercises.js
@@ -20,21 +20,32 @@
 
 // 2.In terms of the properties of books that you thought of, represent the following books as data:
 var book1 = {
-	Title: 'Harry Potter and the Sorcerer\'s Stone',
-	Author: 'J.K.Rowling',
-	MSRP: '6,5 $',
-	Genre: 'fantasy',
-	NumberOfPages:  '223',
-	Description: 'A young wizard that fights a powerful wizard that killed his parents.'
-}
+	title: 'Harry Potter and the Sorcerer\'s Stone',
+	author1: 'J.K.Rowling',
+	author2: 'N/A',
+	msrp: '6,5 $',
+	genre: 'fantasy',
+	numberOfPages:  '223',
+	description: 'A young wizard that fights a powerful wizard that killed his parents.'
+};
 var book2 = {
-	Title: 'Romeo and Juliet',
-	Author: 'William Shakespeare',
-	MSRP: '5,34 $',
-	Genre: 'Romance',
-	NumberOfPages:  '480',
-	Description: 'Two lovers that go through the forbidden love experience';
-}
+	title: 'Romeo and Juliet',
+	author1: 'William Shakespeare',
+	author2: 'N/A',
+	msrp: '5,34 $',
+	genre: 'Romance',
+	numberOfPages:  '480',
+	description: 'Two lovers that go through the forbidden love experience'
+};
+var book3 = {
+	title: 'Structure and Interpretation of Computer Programs',
+	author1: 'Gerald Jay Sussman',
+	author2: 'Hal Abelson',
+	msrp: '51,00 $',
+	genre: 'Textbook',
+	numberOfPages:  '657',
+	description: ' It teaches fundamental principles of computer programming, including recursion, abstraction, modularity, and programming language design and implementation'
+};
 //  Harry Potter and the Sorcerer's Stone (J.K. Rowling)
 //  Romeo and Juliet (William Shakespeare)
 //  Structure and Interpretation of Computer Programs (Gerald Jay Sussman, Hal Abelson)
@@ -42,25 +53,35 @@ var book2 = {
 //  Three other books (see this list for ideas)
 
 // 3.You may have been rewriting the same type of object over and over. We need to stay DRY (Do Not Repeat). Write a function makeBook that takes as arguments different attributes of a book and returns an object representing that book that has the proper structure (we call this a factory function).
-
+function makeBook(title, author1, author2, msrp, genre, numberOfPages, description){
+	return {
+		title: title,
+		author1: author1,
+		author2: author2,
+		msrp: msrp,
+		genre: genre,
+		numberOfPages: numberOfPages,
+		description: description  
+	};
+}
 // 4.Look at one of your book objects in the console. This is the object inspector. The object inspector is nice to have, but it will be easier to have a function to display the more important information easily. Write a function called displayBook that takes a book as an argument, and returns the important information in a more readable way, for example:
 
 // var sorcerersStone = { /* ... */ }
-//  function displayBook(book) {
-//        // ...
-//  }
+function displayBook(book) {
+   return book.title + ', by ' + book.author1 + ' -- ' + book.genre + ', ' + book.msrp;
+}
 //  displayBook(sorcerersStone);
 //  // => 'Harry Potter and the Sorcerer's Stone, by J.K. Rowling -- fantasy, $24.99'
 // The output string above is only an example. What information is most important to you? How can you make that information easier to read for people?
 
 // 5.Create an array called books that holds all of the books that you created above.
-
+var books = [book1, book2, book2];
 // 6.Your function displayBook can be used to display a single book as a string. Now, write a function displayBooks that, given an array of books, returns a single string consisting of all of the books. Use the function displayBook to format all of the books. Each book should be numbered and separated with a newline (we also call this a line break) character so that each book is shown on a separate line in the console. The newline character is specified with a special escaped character in a string:
 
 //  // Enter the below line into a console 'Hello /n World!'; // the 'backslash n' character is a newline
-//  function displayBooks(books) {
-//        // ...
-//  }
+function displayBooks(books) {
+   
+}
 //  displayBooks(books);
 //  // => '1. Harry Potter and the Sorcerer's Stone... /n 2. Snow Crash, ...'
 
